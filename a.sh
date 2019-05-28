681d1c81f6e46754e7333d69b7c15dae5aa7fb55
liangshijian <liangshijian@baidu.com>
2019-05-28 23:12:35 +0800

    添加autosuggest插件

---
+++ a.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+read a
+if [ "$a" == "a" ]  
+then  
+    echo "Not Contains"  
+else  
+    echo "Not Contains"  
+fi
--- bashinit.sh
+++ bashinit.sh
@@ -2,12 +2,24 @@
 #zsh install
 echo "please input you bundle manager,exp : yum / apt-get/ jumbo"
 read bm
+cmd = "sudo yum"
+if [ "$bm" == "jumbo" ]  
+then  
+
+cmd="jumbo"
+else 
+cmd="sudo "$bm
+fi
+echo $cmd
+exit
 #install Oh-my-shell
-sudo $bm install zsh -y
+$cmd install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+#autosuggest
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 cp .zshrc ~/.zshrc
 #install tmux
-sudo $bm install tmux -y
+$cmd install tmux -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux ~/.tmux
 #install vim
